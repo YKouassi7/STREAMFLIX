15a2681dc377a3a811e5236bca28f58a5340ae90
YKouassi7 <yoannekouassi7@gmail.com>
2018-10-06 11:09:11 +0200

    first commit

---
+++ JS/main.js
@@ -0,0 +1,36 @@
+
+		  	$(document).ready(() => {
+			$('#searchForm').on('submit', (e) => {
+ 			let searchText = $('#searchText').val();
+ 			getMovies(searchText);
+ 			e.preventDefault();
+
+ 			 	});
+			});
+
+ 			function getMovies(searchText){
+ 			axios.get('http://www.omdbapi.com/?s='+searchText+'&apikey=364c8c77')
+ 				.then((response) => {
+ 				console.log(response);
+ 				let movies = response.data.Search;
+ 				let output = '';
+ 				$.each(movies, (index, movie) => {
+ 					output += `
+ 				<div class="col-md-3">
+ 				<div class="well text-center">
+ 				<img src="${movie.Poster}">	
+ 				<h5>${movie.Title}</h5>
+ 				<a onclick="movieSelected('${movie.imdb}')" class="btn btn-primary" href="Description"></a>
+ 				</div>	
+ 				</div>
+ 					`; 
+ 				})
+
+ 				$('#movies').html(output);
+ 								
+ 			})
+
+ 			.catch((err) => {
+    			console.log(err);
+ 			 });
+ 			}
